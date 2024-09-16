3c0abf4be9b8cad3b17500c5cae5b079bcb1775f
kkbeta <131790547+kkbeta@users.noreply.github.com>
2024-09-16 10:29:40 +0800

    Update install_ali_200G_limite.sh

--- install_ali_200G_limite.sh
+++ install_ali_200G_limite.sh
@@ -1,6 +1,7 @@
 wget https://raw.githubusercontent.com/kkbeta/ali/main/monitor_traffic.sh
+wget https://raw.githubusercontent.com/kkbeta/ali/main/change_cloudflare_dns_ip_record.sh
 wget https://raw.githubusercontent.com/kkbeta/ali/main/README.md 
 apt-get install -y iptables bc vnstat
-chmod +x monitor_traffic.sh
+chmod +x monitor_traffic.sh change_cloudflare_dns_ip_record.sh
 bash monitor_traffic.sh 190 3
 (crontab -l ; echo "* * * * * /root/monitor_traffic.sh 190 1 4 eth0 > /root/脚本日志.txt") | crontab -
